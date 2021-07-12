2f06e51993ffcd8a31da19d63b008cc2f2c9d9ee
lupei <lupei@wsy.com>
2021-07-12 11:17:47 +0800

    贪吃蛇小游戏

--- useTS/src/moduls/GameControl.ts
+++ useTS/src/moduls/GameControl.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: lu
  * @Date: 2021-07-09 17:19:09
- * @LastEditTime: 2021-07-09 18:49:41
+ * @LastEditTime: 2021-07-12 10:31:38
  * @FilePath: \TypeScript\useTS\src\moduls\GameControl.ts
  * @Description: 
  */
@@ -17,6 +17,9 @@ class GameControl {
     // 创建一个属性来存储按键的方向
     direction: string = '';
 
+    // 创建一个属性用来记录游戏是否结束
+    isLive = true;
+
     constructor() {
         this.snake = new Snake();
         this.food = new Food();
@@ -28,6 +31,9 @@ class GameControl {
     init() {
         // 绑定键盘按键事件
         document.addEventListener('keydown', this.keydownHandler.bind(this))
+
+        this.run();
+
     }
 
     // 创建一个键盘按下的响应函数
@@ -38,7 +44,58 @@ class GameControl {
 
     // 创建一个控制移动的方法
     run() {
+        // 获取当前的坐标
+        let X = this.snake.X;
+        let Y = this.snake.Y;
 
+        // 是否吃到食物
+        this.checkEat(X, Y)
+
+        // 根据按键方向来修改X值和Y值
+        switch (this.direction) {
+            case 'ArrowDown':
+            case 'Down':
+                Y += 10;
+                break;
+            case 'ArrowUp':
+            case 'Up':
+                Y -= 10;
+                break;
+            case 'ArrowLeft':
+            case 'Left':
+                X -= 10;
+                break;
+            case 'ArrowRight':
+            case 'Right':
+                X += 10;
+                break;
+        }
+
+        // 修改X  Y的值
+        try {
+            this.snake.X = X;
+            this.snake.Y = Y
+        } catch (e) {
+            // 进入到catch，说明出现了异常，游戏结束，弹出一个提示
+            alert(e.message + 'GAME OVER!');
+            this.isLive = false;
+        }
+
+
+        // 开启一个定时器
+        this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30)
+    }
+
+    // 定义一个方法，用来检查是否吃到食物
+    checkEat(X: number, Y: number) {
+        if (X == this.food.X && Y == this.food.Y) {
+            // 食物位置进行重置
+            this.food.change();
+            // 分数增加
+            this.scorePanel.addScore();
+            // 蛇增加一节
+            this.snake.addBody();
+        };
     }
 
 }
--- useTS/src/moduls/Snake.ts
+++ useTS/src/moduls/Snake.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: lu
  * @Date: 2021-07-09 17:04:24
- * @LastEditTime: 2021-07-09 17:18:18
+ * @LastEditTime: 2021-07-12 11:15:25
  * @FilePath: \TypeScript\useTS\src\moduls\Snake.ts
  * @Description: 
  */
@@ -25,15 +25,89 @@ class Snake {
         return this.head.offsetTop;
     }
     set X(value: number) {
-        this.head.style.left = value + '';
+        // 如果新值和旧值一样就不设置
+        if (this.X === value) {
+            return;
+        }
+        // X的范围0-290
+        if (value < 0 || value > 290) {
+            throw new Error('蛇撞墙了');
+        }
+        // 修改水平坐标，在左右移动，在向左移动时，不能向右掉头
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            // 如果发生掉头，让蛇向反方向继续移动
+            if (value > this.X) {
+                // 如果新值大于旧值X，则说明蛇向右掉头，应该使他继续向左走
+                value = this.X - 10;
+            } else {
+                value = this.X + 10;
+            }
+
+        }
+
+        this.moveBody();
+        this.head.style.left = value + 'px';
+        this.checkHeadBody();
     }
     set Y(value: number) {
-        this.head.style.top = value + '';
+        // 如果新值和旧值一样就不设置
+        if (this.Y === value) {
+            return;
+        }
+        // X的范围0-290
+        if (value < 0 || value > 290) {
+            throw new Error('蛇撞墙了');
+        }
+        // 修改垂直方向，在上下移动，在向上移动时，不能向下掉头
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            // 如果发生掉头，让蛇向反方向继续移动
+            if (value > this.Y) {
+                // 如果新值大于旧值X，则说明蛇向右掉头，应该使他继续向左走
+                value = this.Y - 10;
+            } else {
+                value = this.Y + 10;
+            }
+
+        }
+        this.moveBody();
+        this.head.style.top = value + 'px';
+        this.checkHeadBody();
     }
 
+    // 增加身体的方法
     addBody() {
         this.element.insertAdjacentHTML('beforeend', '<div></div>')
     }
+
+    // 添加一个身体移动的方法
+    moveBody() {
+        /**
+         * 将后面的身体设置为前边身体的位置
+        */
+        // 遍历获取所有的身体
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            // 获取前边身体的位置
+            let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+
+            // 将值设置到当前身体上
+            (this.bodies[i] as HTMLElement).style.left = X + 'px';
+            (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+        }
+    }
+
+    // 蛇头是否撞到身体
+    checkHeadBody() {
+        // 获取所有的身体，检测其是否和蛇头的坐标发生重叠
+        for (let i = 1; i < this.bodies.length; i++) {
+            let bd = this.bodies[i] as HTMLElement;
+            if (this.X === bd.offsetLeft && this.Y === bd.offsetTop) {
+                // 进入判断说明蛇头撞到了身体，游戏结束
+                throw new Error('撞到自己了~~')
+            }
+        }
+    }
+
 }
 
 export default Snake;
